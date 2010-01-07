6f09082fdff57246421b29417ba34663d43ff591
bbuxton <bbuxton@feb2bf86-fa46-11de-ba51-6b4196a2b81e>
2010-01-07 23:01:09 +0000

    Use native library, also add strobe.

--- trunk/src/net/cactii/flash/MainActivity.java
+++ trunk/src/net/cactii/flash/MainActivity.java
@@ -38,12 +38,13 @@ public class MainActivity extends Activity {
 
 	private Button buttonOff;
 	private Button buttonOn;
+	private Button buttonStrobe;
+	public Boolean strobing;
 	private Button buttonFlash;
+	public Thread strobeThread;
 	private Context context;
 	public SuCommand su_command;
-	
-	private static final String FLASH = "/system/bin/toolbox ioctl -l 1 -a 1 /dev/msm_camera/config0 0x40046d16 ";
-	
+		
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,13 +53,17 @@ public class MainActivity extends Activity {
         context = this.getApplicationContext();
         buttonOff = (Button) findViewById(R.id.buttonOff);
         buttonOn = (Button) findViewById(R.id.buttonOn);
+        buttonStrobe = (Button) findViewById(R.id.buttonStrobe);
+
         buttonFlash = (Button) findViewById(R.id.buttonFlash);
         su_command = new SuCommand();
+        strobing = false;
         
         buttonOff.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if (su_command.Run(FLASH + " 0") == false) {
+				strobing = false;
+				if (setFlashOff().equals("Failed")) {
 					Toast.makeText(context, "Error setting LED off", Toast.LENGTH_LONG).show();
 					return;
 				}
@@ -69,7 +74,8 @@ public class MainActivity extends Activity {
         buttonOn.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if (su_command.Run(FLASH + " 1") == false) {
+				strobing = false;
+				if (setFlashOn().equals("Failed")) {
 					Toast.makeText(context, "Error setting LED on", Toast.LENGTH_LONG).show();
 					return;
 				}
@@ -77,10 +83,40 @@ public class MainActivity extends Activity {
 			}
         });
         
+        buttonStrobe.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				strobeThread = new Thread(new Runnable() {
+					@Override
+					public void run() {
+						strobing = true;
+						while (strobing && !Thread.interrupted()) {
+							setFlashOn();
+							try {
+								Thread.sleep(100);
+							} catch (InterruptedException e) {
+								setFlashOff();
+							}
+							if (strobing)
+								setFlashOff();
+							try {
+								Thread.sleep(100);
+							} catch (InterruptedException e) {
+								setFlashOff();
+							}
+						}
+						strobing = false;
+					}
+				});
+				strobeThread.start();
+			}
+        });
+        
         buttonFlash.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				if (su_command.Run(FLASH + " 2") == false) {
+				strobing = false;
+				if (setFlashFlash().equals("Failed")) {
 					Toast.makeText(context, "Error setting LED flash", Toast.LENGTH_LONG).show();
 					return;
 				}
@@ -93,26 +129,44 @@ public class MainActivity extends Activity {
         if (new File("/dev/msm_camera/config0").exists() == false) {
         	Toast.makeText(context, "Only Nexus One is supported, sorry!", Toast.LENGTH_LONG).show();
         	is_supported = false;
-        }
-        
-        is_supported = this.su_command.can_su;
-         
-    	if (new File("/system/bin/ioctl").exists() == false) {
-    		Toast.makeText(context, "Cant find 'ioctl' binary, sorry!", Toast.LENGTH_LONG).show();
-    		is_supported = false;
-    	}
+        } else 
+        	is_supported = this.su_command.can_su;
         
         if (!is_supported) {
         	buttonOff.setEnabled(false);
         	buttonOn.setEnabled(false);
         	buttonFlash.setEnabled(false);
+        } else {
+        	su_command.Run("chmod 666 /dev/msm_camera/config0");
         }
     }
     
-    public void onStop() {
-    	su_command.Run(FLASH + " off");
-    	super.onStop();
+    public void onPause() {
+    	strobing = false;
+    	if (strobeThread != null) {
+	    	try {
+				strobeThread.join();
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+    	}
+    	setFlashOff();
+    	closeFlash();
+    	super.onPause();
     }
+    
+    public void onResume() {
+    	openFlash();
+    	super.onResume();
+    }
+    
+    public native String  openFlash();
+    public native String  setFlashOff();
+    public native String  setFlashOn();
+    public native String  setFlashFlash();
+    public native String  closeFlash();
+
     
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -180,5 +234,8 @@ public class MainActivity extends Activity {
    			}
    	    	return false;
    	    }	
+   	}
+   	static {
+   		System.loadLibrary("flash");
    	}
 }
